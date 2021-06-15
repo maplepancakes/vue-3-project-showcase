5c91f177282591fe9c701ffd8cb1812eba62e8bf
maplepancakes <ngsiohongssg@gmail.com>
2021-06-15 21:34:38 +0800

    imported vue router for use

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import "./assets/style.css";
+import router from './router/router.js';
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router);
+
+app.mount('#app');
+
