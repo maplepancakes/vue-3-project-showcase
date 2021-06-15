d54d572f04d88f2c2360c95c3beef48c486a5f1a
maplepancakes <ngsiohongssg@gmail.com>
2021-06-15 21:55:39 +0800

    Added route linkage for markdown application page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,8 @@
 <template>
-    <nav class="border-b p-2 flex justify-evenly">
-        <router-link to="/listApp">List App</router-link>
-        <router-link to="/calendarApp">Calendar App</router-link>
+    <nav class="border-b p-2 flex justify-evenly flex-wrap">
+        <router-link to="/listApp" class="hover:text-gray-400">List App</router-link>
+        <router-link to="/calendarApp" class="hover:text-gray-400">Calendar App</router-link>
+        <router-link to="/markdownApp" class="hover:text-gray-400">Markdown App</router-link>
     </nav>
 </template>
 
