5d36d5d4d796db36e1f247efbe4df89678c45769
maplepancakes <ngsiohongssg@gmail.com>
2021-06-15 22:04:16 +0800

    Added transition animations

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="border-t w-full absolute bottom-0 p-5 hover:text-gray-400">
+  <footer class="border-t w-full absolute bottom-0 p-5 hover:text-gray-400 transition ease-out duration-500">
       <router-link to="/">Back to Home</router-link>
   </footer>
 </template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="border-b p-2 flex justify-evenly flex-wrap">
-        <router-link to="/listApp" class="hover:text-gray-400">List App</router-link>
-        <router-link to="/calendarApp" class="hover:text-gray-400">Calendar App</router-link>
-        <router-link to="/markdownApp" class="hover:text-gray-400">Markdown App</router-link>
+        <router-link to="/listApp" class="hover:text-gray-400 transition ease-out duration-500">List App</router-link>
+        <router-link to="/calendarApp" class="hover:text-gray-400 transition ease-out duration-500">Calendar App</router-link>
+        <router-link to="/markdownApp" class="hover:text-gray-400 transition ease-out duration-500">Markdown App</router-link>
     </nav>
 </template>
 
