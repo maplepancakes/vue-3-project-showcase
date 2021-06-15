4a133ceef96de48cd49cf1c58f8a403dc0db2ad4
maplepancakes <ngsiohongssg@gmail.com>
2021-06-15 22:06:39 +0800

    Fixed footer transition

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="border-t w-full absolute bottom-0 p-5 hover:text-gray-400 transition ease-out duration-500">
-      <router-link to="/">Back to Home</router-link>
+  <footer class="border-t w-full absolute bottom-0 p-5">
+      <router-link to="/" class="hover:text-gray-400 transition ease-out duration-500">Back to Home</router-link>
   </footer>
 </template>
 
