94d976c12bb2b27fbf39c5bead134c8cf6f1cd46
maplepancakes <ngsiohongssg@gmail.com>
2021-06-15 21:54:49 +0800

    Added footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="border-t w-full absolute bottom-0 p-5 hover:text-gray-400">
+      <router-link to="/">Back to Home</router-link>
+  </footer>
+</template>
+
+<script>
+export default 
+{
+
+}
+</script>
+
+<style>
+
+</style>
