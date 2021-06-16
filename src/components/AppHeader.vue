2c01ba8748dd4c5e37d74ff74e4ee4d7f13705e4
maplepancakes <ngsiohongssg@gmail.com>
2021-06-16 18:13:42 +0800

    Addition of routerList array, used to generate items for navigation bar for routing purposes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,22 @@
 <template>
     <nav class="border-b p-2 flex justify-evenly flex-wrap">
-        <router-link to="/listApp" class="hover:text-gray-400 transition ease-out duration-500">List App</router-link>
-        <router-link to="/calendarApp" class="hover:text-gray-400 transition ease-out duration-500">Calendar App</router-link>
-        <router-link to="/markdownApp" class="hover:text-gray-400 transition ease-out duration-500">Markdown App</router-link>
+        <router-link class="hover:text-gray-400 transition ease-out duration-500" v-for="router in routerList" :key="router.to" :to="router.to">{{ router.title }}</router-link>
     </nav>
 </template>
 
 <script>
 export default 
 {
-
+    data()
+    {
+        return {
+            routerList:
+            [
+                {to: "/listApp", title: "List App"},
+                {to: "/calendarApp", title: "Calendar App"},
+                {to: "/markdownApp", title: "Markdown App"},
+            ]
+        }
+    }
 }
 </script>
