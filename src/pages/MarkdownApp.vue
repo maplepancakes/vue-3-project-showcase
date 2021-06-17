10cecefc54fe9b281bfa2f49dacba9216a202ff1
maplepancakes <ngsiohongssg@gmail.com>
2021-06-17 19:39:09 +0800

    Added div wrapper element

--- src/pages/MarkdownApp.vue
+++ src/pages/MarkdownApp.vue
@@ -1,11 +1,13 @@
 <template>
-    <h1 class="text-3xl text-center font-bold text-gray-400 mt-20">Markdown App</h1>
-    <div class="h-96 flex justify-between m-5">
-        <textarea v-model="textInput" class="w-1/2 border mr-1 p-2 resize-none" placeholder="Input your markdown text here! (e.g. **This is a bold text**)"></textarea>
-        <article id="output" class="w-1/2 border ml-1 p-2"></article>
+    <div>
+        <h1 class="text-3xl text-center font-bold text-gray-400 mt-20">Markdown App</h1>
+        <div class="h-96 flex justify-between m-5">
+            <textarea v-model="textInput" class="w-1/2 border mr-1 p-2 resize-none" placeholder="Input your markdown text here! (e.g. **This is a bold text**)"></textarea>
+            <article id="output" class="w-1/2 border ml-1 p-2"></article>
+        </div>
+        <button @click="markTextInput()" class="ml-5 w-28 border text-gray-400 p-2 focus:outline-none hover:bg-gray-100 transition ease-out duration-500">Convert</button>
+        <button @click="resetInput()" class="ml-5 w-28 border text-gray-400 p-2 focus:outline-none hover:bg-gray-100 transition ease-out duration-500">Reset</button>
     </div>
-    <button @click="markTextInput()" class="ml-5 w-28 border text-gray-400 p-2 focus:outline-none hover:bg-gray-100 transition ease-out duration-500">Convert</button>
-    <button @click="resetInput()" class="ml-5 w-28 border text-gray-400 p-2 focus:outline-none hover:bg-gray-100 transition ease-out duration-500">Reset</button>
 </template>
 
 <script>
