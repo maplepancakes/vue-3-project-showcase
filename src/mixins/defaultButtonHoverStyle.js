500afdcf47630dd7bf6761bfb193c609da647ddd
maplepancakes <ngsiohongssg@gmail.com>
2021-06-22 20:57:05 +0800

    Mixin for default button hover style

---
+++ src/mixins/defaultButtonHoverStyle.js
@@ -0,0 +1,10 @@
+export default
+{
+    methods:
+    {
+        defaultButtonHoverStyle()
+        {
+            return "focus:outline-none hover:bg-gray-100 transition ease-out duration-500";
+        }
+    }
+}
