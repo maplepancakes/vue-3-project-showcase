dce0e67b3acf789311155712dd7379c366e70fe6
maplepancakes <ngsiohongssg@gmail.com>
2021-06-22 20:55:37 +0800

    Added login button with event emitter

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,10 @@
 <template>
-    <nav class="border-b p-2 flex justify-evenly flex-wrap">
-        <router-link class="hover:text-gray-400 transition ease-out duration-500" v-for="router in routerList" :key="router.to" :to="router.to">{{ router.title }}</router-link>
-    </nav>
+    <div class="flex w-full">
+        <nav class="border-b border-r p-2 flex justify-evenly flex-wrap w-11/12">
+            <router-link class="hover:text-gray-400 transition ease-out duration-500" v-for="router in routerList" :key="router.to" :to="router.to">{{ router.title }}</router-link>
+        </nav>
+        <button @click="$emit('open-login-screen')" class="text-gray-400 border-b p-2 w-1/12 focus:outline-none hover:bg-gray-100 transition ease-out duration-500">Login</button>
+    </div>
 </template>
 
 <script>
@@ -18,6 +21,6 @@ export default
                 {to: "/sliderApp", title: "Slider App"},
             ]
         }
-    }
+    },
 }
 </script>
