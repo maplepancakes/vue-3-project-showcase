3333b9e14f0c0f26114917e994cb8014720124e8
maplepancakes <ngsiohongssg@gmail.com>
2021-06-24 15:32:19 +0800

    Added enter event for submitting login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -7,13 +7,14 @@
                     <div class="p-5 text-sm">
                         <form @submit.prevent="onSubmit" class="mb-2">   
                             <label for="input-user">Email or Username</label>
-                            <input v-model="form.user" :class="defaultInputStyle()" id="input-user" class="w-96" type="text" placeholder="Enter your email or username">
+                            <input v-model="form.user" @keydown.enter="this.submit()" :class="defaultInputStyle()" id="input-user" class="w-96" type="text" placeholder="Enter your email or username">
                         </form>
                         <form @submit.prevent="onSubmit" class="mb-2">
                             <label for="input-password">Password</label>
-                            <input v-model="form.password" :class="defaultInputStyle()" id="input-password" class="w-96" type="password" placeholder="Enter your password">
+                            <input v-model="form.password" @keydown.enter="this.submit()" :class="defaultInputStyle()" id="input-password" class="w-96" type="password" placeholder="Enter your password">
                         </form>
-                        <button @click="this.submit()" :class="defaultButtonHoverStyle()" id="login-button" class="border p-1 w-96" type="submit"></button>
+                        <button @click="this.submit()" :class="defaultButtonHoverStyle()" id="login-button" class="border p-1 mb-2 w-96" type="submit"></button>
+                        <label v-if="invalidInput" class="text-red-400">Invalid email or password!</label>
                     </div>
                 </div>
             </div>
@@ -39,7 +40,8 @@ export default
                 user: "",
                 password: "",
             },
-            loginButtonText: ["Login", "Signing in...", "Login Success!"]
+            loginButtonText: ["Login", "Signing in...", "Login Success!"],
+            invalidInput: false,
         }
     },
     methods:
@@ -51,6 +53,8 @@ export default
             .signInWithEmailAndPassword(this.form.user, this.form.password)
             .then(() =>
             {
+                this.invalidInput = false;
+
                 const loginButton = this.selectElement("#login-button");
                 loginButton.textContent = this.loginButtonText[1];
 
@@ -76,11 +80,14 @@ export default
                 console.log("Sign In Error Code: ", errorCode);
                 console.log("Sign In Error Message: ", errorMessage);
 
+                this.invalidInput = true;
+
                 const inputUser = this.selectElement("#input-user");
                 const inputPassword = this.selectElement("#input-password");
 
-                inputUser.style.borderColor = "red";
-                inputPassword.style.borderColor = "red";
+                inputUser.classList.add("border-red-400");
+                inputPassword.classList.add("border-red-400");
+
             });
         }
     },
