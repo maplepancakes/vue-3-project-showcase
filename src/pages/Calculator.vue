a0ec47aab9c95b80f3d51b5997cdaf087ebff998
maplepancakes <ngsiohongssg@gmail.com>
2021-06-30 22:27:14 +0800

    Completed calculator application

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -9,23 +9,23 @@
                 <button ref="seven" @click="updateDisplay('7')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">7</button>
                 <button ref="eight" @click="updateDisplay('8')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">8</button>
                 <button ref="nine" @click="updateDisplay('9')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">9</button>
-                <button ref="divide" @click="division()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">/</button>
+                <button ref="divide" @click="operate('/')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">/</button>
                 <button ref="four" @click="updateDisplay('4')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">4</button>
                 <button ref="five" @click="updateDisplay('5')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">5</button>
                 <button ref="six" @click="updateDisplay('6')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">6</button>
-                <button ref="multiply" @click="multiplication()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">*</button>
+                <button ref="multiply" @click="operate('*')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">*</button>
                 <button ref="one" @click="updateDisplay('1')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">1</button>
                 <button ref="two" @click="updateDisplay('2')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">2</button>
                 <button ref="three" @click="updateDisplay('3')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">3</button>
-                <button ref="add" @click="addition()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">+</button>
+                <button ref="add" @click="operate('+')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">+</button>
                 <button ref="zero" @click="updateDisplay('0')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">0</button>
                 <button ref="equal" @click="equals()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">=</button>
                 <button ref="zero" @click="clear()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">CLEAR</button>
-                <button ref="minus" @click="subtraction()" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">-</button>
+                <button ref="minus" @click="operate('-')" :class="defaultButtonHoverStyle()" class="border shadow w-full h-12">-</button>
             </div>
       </div>
   </div>
-</template>
+</template>7
 
 <script>
 import defaultButtonHoverStyle from "../mixins/defaultButtonHoverStyle.js";
@@ -38,7 +38,9 @@ export default
     {
         const display = ref("0");
 
-        let calculatingValue = 0;
+        let storedValue = 0;
+        let operator = "";
+        let operatorInUse = false;
 
         function updateDisplay(value)
         {
@@ -47,54 +49,97 @@ export default
                 return;
             }
 
-            if (display.value === "0")
+            if (display.value === "0" || operatorInUse)
             {
                 display.value = "";
+                operatorInUse = false;
             }
 
             display.value = display.value + value;
         }
 
-        function clear()
-        {
-            display.value = "0";
-            calculatingValue = 0;
+        function calculate()
+        {       1
+            if (operator === "")
+            {
+                return;
+            }
+            else if (operator === "+")
+            {
+                display.value = storedValue + parseInt(display.value); // add
+            }
+            else if (operator === "-")
+            {
+                display.value = storedValue - parseInt(display.value); // subtract
+            }
+            else if (operator === "*")
+            {
+                display.value = storedValue * parseInt(display.value); // multiply
+            }
+            else if (operator === "/")
+            {
+                display.value = storedValue / parseInt(display.value); // divide
+            }
+
+            if (Number.isNaN(parseInt(display.value)))
+            {
+                display.value = "0";
+            }
         }
 
-        function addition()
+        function operate(value)
         {
-            display.value = parseInt(display.value) + calculatingValue;
-        }
+            if (parseInt(display.value) === storedValue && operatorInUse)
+            {
+                return;
+            }
 
-        function subtraction()
-        {
-               
-        }
+            operatorInUse = true;
+            
+            if (storedValue === 0)
+            {
+                storedValue = parseInt(display.value);
+                operator = value;
 
-        function multiplication()
-        {
+                return;
+            }
 
-        }
-        
-        function division()
-        {
+            calculate();
 
+            storedValue = parseInt(display.value);
+            operator = value;
         }
 
         function equals()
         {
+            if (parseInt(display.value) === storedValue && operatorInUse)
+            {
+                return;
+            }
             
+            calculate();
+
+            storedValue = 0;
+            operator = "";
+        }
+
+        function clear()
+        {
+            display.value = "0";
+            storedValue = 0;
+            operatorInUse = false;
+            operator = "";
         }
 
         return  {
             display,
-            calculatingValue,
+            storedValue,
+            operatorInUse,
+            operator,
+            calculate,
+            operate,
             updateDisplay,
             clear,
-            addition,
-            subtraction,
-            multiplication,
-            division,
             equals,
         };
     }
