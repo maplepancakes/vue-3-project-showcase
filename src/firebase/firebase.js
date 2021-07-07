a706ea6ce5942a51692db82917e29ac54e66a2bf
maplepancakes <ngsiohongssg@gmail.com>
2021-07-07 09:35:04 +0800

    Addition of firebase real-time database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
 apiKey: "AIzaSyBkhcti8m-CCgwEe8jaGPI1B5FJ6hkMwXU",
@@ -7,7 +8,8 @@ authDomain: "vue-3-project-showcase.firebaseapp.com",
 projectId: "vue-3-project-showcase",
 storageBucket: "vue-3-project-showcase.appspot.com",
 messagingSenderId: "439050846293",
-appId: "1:439050846293:web:0b747c6c902ef77dd594dc"
+appId: "1:439050846293:web:0b747c6c902ef77dd594dc",
+databaseURL: "https://vue-3-project-showcase-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
 firebase.initializeApp(firebaseConfig);
