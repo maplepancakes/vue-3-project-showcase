f2f9b213ecc499637c8ed46d159f95a8bba1c7c3
maplepancakes <ngsiohongssg@gmail.com>
2021-07-07 16:37:19 +0800

    Modified writeDataToFirebase() function

--- src/pages/ChatApp.vue
+++ src/pages/ChatApp.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="mx-10">
         <h1 class="text-3xl text-center font-bold text-gray-400 mt-20">Chat App</h1>
-        <div class="mx-10 mt-5 h-96 border">
+        <div ref="chatDisplay" class="mt-5 h-96 border overflow-y-scroll overflow-x-hidden break-words">
             <div v-for="message in messages" :key="message" class="p-2">{{ message.message }}</div>
         </div>
-        <input ref="chatInput" @keydown.enter="writeDataToFirebase()" :class="defaultInputStyle()" class="mx-10 mt-5"/>
+        <input ref="chatInput" @keydown.enter="writeDataToFirebase()" :class="defaultInputStyle()" class="mt-5 w-full" placeholder="Start Typing!"/>
     </div>
 </template>
 
@@ -17,20 +17,49 @@ import FirebaseMethods from "../composition/FirebaseMethods.js";
 export default 
 {
     mixins: [defaultInputStyle],
+    props:
+    {
+        isLoggedIn:
+        {
+            type: Boolean,
+        }
+    },
     setup()
     {
         const messages = ref({});
         const chatInput = ref("");
         const chatID = ref({});
+        const chatDisplay = ref("");
 
         function writeDataToFirebase()
         {
-            firebase.database().ref("chats/" + "M" + chatID.value).set(
+            try
+            {
+                // retrieves currently logged in userID
+                let userID = firebase.auth().currentUser.uid;
+
+                // updates "chats" tree in firebase
+                firebase.database().ref("chats/" + chatID.value.toString().padStart(20, "0")).set(
                 {
+                    userID: userID,
                     message: chatInput.value.value,
                 });
             
-            firebase.database().ref("chatIDCounter/").set(chatID.value + 1);
+                // updates "chatIDCounter" in firebase
+                firebase.database().ref("chatIDCounter/").set(chatID.value + 1);
+
+                // scrolls to bottom of div everytime a new message is written to firebase
+                setTimeout(() => 
+                {
+                    chatDisplay.value.scrollTop = chatDisplay.value.scrollHeight;
+                }, 0);
+            }
+            catch (error)
+            {
+                alert("Logging in is required for usage of chat application.");
+
+                console.log(error);
+            }
             
             chatInput.value.value = "";
         }
@@ -45,6 +74,7 @@ export default
             messages,
             chatInput,
             chatID,
+            chatDisplay,
             writeDataToFirebase,
         }
     },
